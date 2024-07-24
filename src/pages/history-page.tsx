6c4ab32806cb17224e1d87964d32a5ce25e78a85
Julien THOMAS <125746558+doncarlo5@users.noreply.github.com>
2024-07-24 17:25:36 +0200

    history: add sorting

--- src/pages/history-page.tsx
+++ src/pages/history-page.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { format } from "date-fns"
-import { ArrowRight, MessageSquareText, Plus } from "lucide-react"
+import { ArrowDown, ArrowRight, ArrowUp, MessageSquareText, Plus } from "lucide-react"
 import { useNavigate } from "react-router-dom"
 
 import fetchApi from "@/lib/api-handler"
@@ -15,13 +15,15 @@ export function HistoryPage() {
   const [tab, setTab] = useState("session")
   const [sessions, setSessions] = useState([] as any[])
   const [exercises, setExercises] = useState([] as any[])
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(false)
+  const [sortConfig, setSortConfig] = useState({ field: "date_session", order: "desc" })
 
   const navigate = useNavigate()
 
   const fetchUserSessions = async () => {
     try {
-      const response = await fetchApi("/api/sessions?limit=1000&sortBy=date_session:desc");
+      setIsLoading(true)
+      const response = await fetchApi(`/api/sessions?limit=1000&sortBy=${sortConfig.field}:${sortConfig.order}`)
       setSessions(response)
     } catch (error: any) {
       console.error("Fetch error: ", error)
@@ -44,7 +46,26 @@ export function HistoryPage() {
   useEffect(() => {
     fetchUserSessions()
     fetchUserExercises()
-  }, [])
+  }, [sortConfig])
+
+  const handleSort = (field: string) => {
+    let order = "asc"
+    if (sortConfig.field === field && sortConfig.order === "asc") {
+      order = "desc"
+    }
+    setSortConfig({ field, order })
+  }
+
+  const getSortIcon = (field: string) => {
+    if (sortConfig.field !== field) {
+      return null
+    }
+    return sortConfig.order === "asc" ? (
+      <ArrowUp size={12} className="ml-1 inline-block" />
+    ) : (
+      <ArrowDown size={12} className="ml-1 inline-block" />
+    )
+  }
 
   return (
     <div>
@@ -59,96 +80,89 @@ export function HistoryPage() {
             <TabsTrigger value="exercise">Exercices</TabsTrigger>
           </TabsList>
           <TabsContent value="session">
-            {isLoading ? (
-              Array.from({ length: 15 }).map((_, index) => (
-                <div key={index} className="my-2">
-                  <Skeleton className="h-10 w-full rounded-lg" />
-                </div>
-              ))
-            ) : (
-              <>
-                <Table>
-                  <TableCaption>Total de {sessions.length} séances.</TableCaption>
-                  <TableHeader>
-                    <TableRow>
-                      <TableHead></TableHead>
-                      <TableHead></TableHead>
-                      <TableHead>Date</TableHead>
-                      <TableHead className=" text-center">Type</TableHead>
-                      <TableHead className=" text-center">Exos</TableHead>
-                      <TableHead></TableHead>
-                    </TableRow>
-                  </TableHeader>
-                  <TableBody>
-                    {sessions.length === 0 ? (
-                      <TableRow>
-                        <TableCell colSpan={6} className="text-center">
-                          Aucun séance.
+            <>
+              <Table>
+                <TableCaption>Total de {sessions.length} séances.</TableCaption>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead></TableHead>
+                    <TableHead></TableHead>
+                    <TableHead onClick={() => handleSort("date_session")} className="cursor-pointer">
+                      Date {getSortIcon("date_session")}
+                    </TableHead>
+                    <TableHead onClick={() => handleSort("type_session")} className="cursor-pointer text-center">
+                      Type {getSortIcon("type_session")}
+                    </TableHead>
+                    <TableHead className="text-center">Exos</TableHead>
+                    <TableHead></TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {isLoading ? (
+                    Array.from({ length: 15 }).map((_, index) => (
+                      <TableRow key={index}>
+                        <TableCell colSpan={6}>
+                          <Skeleton className="h-6 w-full rounded-lg" />
                         </TableCell>
                       </TableRow>
-                    ) : (
-                      sessions.map((oneSession) => (
-                        <TableRow
-                          key={oneSession._id}
-                          className="group cursor-pointer"
-                          onClick={() => navigate(`/history/session/${oneSession._id}`)}
-                        >
-                          <TableCell>
-                            <div
-                              className={`h-2.5 w-2.5 rounded-full ${
-                                oneSession.is_done ? "bg-green-500" : "bg-orange-500"
-                              }`}
-                            ></div>
-                          </TableCell>
-                          <TableCell>
-                            <div>
-                              {oneSession.comment ? (
-                                <MessageSquareText size={16} />
-                              ) : (
-                                <MessageSquareText className="text-slate-200" size={16} />
-                              )}
-                            </div>
-                          </TableCell>
-                          <TableCell>{format(new Date(oneSession.date_session), "dd/MM/yyyy")}</TableCell>
-                          <TableCell className=" text-center">
-                            <Badge
-                              variant={
-                                oneSession.type_session === "Upper A"
-                                  ? "upperA"
-                                  : oneSession.type_session === "Upper B"
-                                    ? "upperB"
-                                    : oneSession.type_session === "Lower"
-                                      ? "lower"
-                                      : oneSession.type_session === "Séance A"
-                                        ? "seanceA"
-                                        : oneSession.type_session === "Séance B"
-                                          ? "seanceB"
-                                          : "default" // Fallback variant in case none of the conditions match
-                              }
-                            >
-                              {oneSession.type_session}
-                            </Badge>
-                          </TableCell>
-                          <TableCell className="text-center">{oneSession.exercise_user_list.length}</TableCell>
-                          <TableCell className="items-center justify-center">
-                            <ArrowRight className="text-slate-300 group-hover:text-slate-900" size={18} />
-                          </TableCell>
-                        </TableRow>
-                      ))
-                    )}
-                  </TableBody>
-                </Table>
-                {sessions.length === 0 && (
-                  <div className="flex flex-1 items-center justify-center">
-                    <div className="container flex flex-col items-center justify-center gap-4 px-4 md:px-6">
-                      <div className="text-center">
-                        <p className="max-w-[600px] py-5 text-gray-500 dark:text-gray-400 md:text-xl">Aucune séance.</p>
-                      </div>
-                    </div>
-                  </div>
-                )}
-              </>
-            )}
+                    ))
+                  ) : sessions.length === 0 ? (
+                    <TableRow>
+                      <TableCell colSpan={6} className="text-center">
+                        Aucun séance.
+                      </TableCell>
+                    </TableRow>
+                  ) : (
+                    sessions.map((oneSession) => (
+                      <TableRow
+                        key={oneSession._id}
+                        className="group cursor-pointer"
+                        onClick={() => navigate(`/history/session/${oneSession._id}`)}
+                      >
+                        <TableCell>
+                          <div
+                            className={`h-2.5 w-2.5 rounded-full ${oneSession.is_done ? "bg-green-500" : "bg-orange-500"}`}
+                          ></div>
+                        </TableCell>
+                        <TableCell>
+                          <div>
+                            {oneSession.comment ? (
+                              <MessageSquareText size={16} />
+                            ) : (
+                              <MessageSquareText className="text-slate-200" size={16} />
+                            )}
+                          </div>
+                        </TableCell>
+                        <TableCell>{format(new Date(oneSession.date_session), "dd/MM/yyyy")}</TableCell>
+                        <TableCell className="text-center">
+                          <Badge
+                            variant={
+                              oneSession.type_session === "Upper A"
+                                ? "upperA"
+                                : oneSession.type_session === "Upper B"
+                                  ? "upperB"
+                                  : oneSession.type_session === "Lower"
+                                    ? "lower"
+                                    : oneSession.type_session === "Séance A"
+                                      ? "seanceA"
+                                      : oneSession.type_session === "Séance B"
+                                        ? "seanceB"
+                                        : "default"
+                            }
+                          >
+                            {oneSession.type_session}
+                          </Badge>
+                        </TableCell>
+                        <TableCell className="text-center">{oneSession.exercise_user_list.length}</TableCell>
+                        <TableCell className="items-center justify-center">
+                          <ArrowRight className="text-slate-300 group-hover:text-slate-900" size={18} />
+                        </TableCell>
+                      </TableRow>
+                    ))
+                  )}
+                </TableBody>
+              </Table>
+            </>
           </TabsContent>
           <TabsContent value="exercise">
             {isLoading ? (
