b4495bec334c1e56b77c3b653ef4a7f634d926f4
Julien THOMAS <125746558+doncarlo5@users.noreply.github.com>
2024-08-25 17:21:05 +0200

    feature: feedback

--- src/components/dashboard-component.tsx
+++ src/components/dashboard-component.tsx
@@ -1,26 +1,73 @@
+import { useState } from "react"
 import useAuth from "@/context/use-auth"
 import {
   LucideActivity,
   LucideLineChart,
   LucideLogOut,
   LucidePencilRuler,
+  LucideStar,
   LucideTimer,
   LucideTrophy,
   LucideUserRoundCog,
-  Video,
 } from "lucide-react"
 import { Link } from "react-router-dom"
 
-// import { useTheme } from "./theme-provider"
+import fetchApi from "@/lib/api-handler"
+
 import { Button } from "./ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
+import { Input } from "./ui/input"
+import { Label } from "./ui/label"
+import { Textarea } from "./ui/textarea"
+import { toast } from "./ui/use-toast"
 
 function DashboardComponent() {
-  const { handleLogout } = useAuth()
-  // const { theme, setTheme } = useTheme()
+  const { user, handleLogout } = useAuth()
+  const [open, setOpen] = useState(false)
+  const [formState, setFormState] = useState({ subject: "", message: "" })
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
 
-  // const toggleTheme = () => {
-  //   setTheme(theme === "light" ? "dark" : "light")
-  // }
+  const handleFeedbackSubmit = async (e: any) => {
+    e.preventDefault()
+    setLoading(true)
+    setError(null)
+
+    try {
+      if (!formState.subject || !formState.message) {
+        throw new Error("Veuillez remplir tous les champs.")
+      }
+      await fetchApi("/api/feedbacks", {
+        method: "POST",
+        body: JSON.stringify({
+          userId: user ? user._id : null,
+          subject: formState.subject,
+          message: formState.message,
+        }),
+      })
+
+      toast({
+        title: "Feeback envoyé!",
+        description: "Merci pour votre contribution.",
+        variant: "success",
+      })
+      setFormState({ subject: "", message: "" })
+      setOpen(false)
+    } catch (error: any) {
+      setError(error.message)
+      console.error(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
 
   return (
     <div className="">
@@ -78,11 +125,78 @@ function DashboardComponent() {
           <LucideLogOut strokeWidth={1.1} size={31} />
           Déconnexion
         </Button>
-        <Button variant={"outline"} className="h-24 cursor-not-allowed opacity-50">
-          <div className="flex flex-col items-center justify-center gap-1">
-            <Video strokeWidth={1.1} size={32} />À venir...
-          </div>
-        </Button>
+
+        {/* Feedback Button and Dialog */}
+        <Dialog open={open} onOpenChange={setOpen}>
+          <DialogTrigger asChild>
+            <Button variant={"outline"} className="flex h-24 flex-col gap-1">
+              <LucideStar strokeWidth={1.1} size={32} />
+              Feedback
+            </Button>
+          </DialogTrigger>
+          <DialogContent className=" w-11/12 rounded-2xl">
+            <DialogHeader>
+              <DialogTitle className="text-left">Feedback</DialogTitle>
+              <DialogDescription className="text-left">Décrivez votre expérience de l'application.</DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-2">
+              {error && <p className="text-sm text-red-500">{error}</p>}
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="subject" className="text-right">
+                  Sujet
+                </Label>
+                <Input
+                  id="subject"
+                  value={formState.subject}
+                  onChange={(e) => setFormState({ ...formState, subject: e.target.value })}
+                  required
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="message" className="text-right">
+                  Message
+                </Label>
+                <Textarea
+                  id="message"
+                  value={formState.message}
+                  onChange={(e) => setFormState({ ...formState, message: e.target.value })}
+                  required
+                  className="col-span-3"
+                />
+              </div>
+              {/* Buttons "Bug", "Feature" shortchuts to type directly in text area */}
+              <div className="flex justify-evenly gap-2">
+                <Button
+                  variant="outline"
+                  onClick={() => setFormState({ ...formState, message: formState.message + "\n• Bug: " })}
+                  className="text-xs"
+                >
+                  Bug
+                </Button>
+                <Button
+                  variant="outline"
+                  onClick={() => setFormState({ ...formState, message: formState.message + "\n• Feature: " })}
+                  className="text-xs"
+                >
+                  Feature
+                </Button>
+                <Button
+                  variant="outline"
+                  onClick={() => setFormState({ ...formState, message: formState.message + "\n• Séance: " })}
+                  className="text-xs"
+                >
+                  Séance
+                </Button>
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit" onClick={handleFeedbackSubmit} disabled={loading}>
+                {loading ? "Envoi en cours..." : "Envoyer"}
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   )
